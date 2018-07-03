291e5468f3418e2738fc17331dd4698f3fba9d63
ALLEN KUANG (ITA-ISDC-ISD-OOCLL/ZHA) <KUANGAL2@OOCL.COM>
2018-07-03 14:49:38 +0800

    practice4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var result= new Array();
+  console.log(collectionA);
+  for(var i=0;i<collectionA.length;i++){
+    for(var k=0;k<objectB.value.length;k++){
+      if(collectionA[i].key==objectB.value[k]){
+        result.push(collectionA[i].key);
+      }
+    }
+  }
+  return result;
 }
